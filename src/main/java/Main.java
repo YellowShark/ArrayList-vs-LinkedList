b76b0d3613e03ea90dcd75014637263dd22eb9aa
Daniil <danil.dobrodeev@mail.ru>
2021-11-16 22:51:52 +0300

    handling errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,49 @@ import list.LinkedListManipulation;
 import printer.Printer;
 
 import java.util.ArrayDeque;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         var in = new Scanner(System.in);
         System.out.println("Input number of experiments:");
-        int count = in.nextInt();
-        var allOperations = new ArrayDeque<Integer>();
-        for (int i = 0; i < count; i++) {
-            System.out.println("Input number of operations for " + (i + 1) + " experiment");
-            allOperations.add(in.nextInt());
+        var count = 0;
+
+        try {
+            count = in.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("Invalid input.");
         }
 
-        Printer.separateLines();
-        for (int i = 0; i < count; i++) {
-            fillInsertRemoveStatistic(allOperations.pop());
+        if (count > 0) {
+            var allOperations = new ArrayDeque<Integer>();
+            int i = 0;
+            while (i < count) {
+                System.out.println("Input number of elements for " + (i + 1) + " experiment.");
+                var input = 0L;
+
+                try {
+                    input = in.nextLong();
+                    if (input > 500_000)
+                        System.out.println("Too many elements. Input other value.");
+                    else {
+                        allOperations.add(Math.toIntExact(input));
+                        ++i;
+                    }
+                } catch (InputMismatchException ex) {
+                    System.out.println("Invalid input.");
+                }
+            }
+
+            Printer.separateLines();
+            for (i = 0; i < count; i++) {
+                executeExperiment(allOperations.pop());
+            }
         }
     }
 
-    private static void fillInsertRemoveStatistic(int listSize) {
+    private static void executeExperiment(int listSize) {
         LinkedListManipulation linkedList = new LinkedListManipulation();
         ArrayListManipulation arrayList = new ArrayListManipulation(listSize);
 
